bdf6eac215fa2d5da95217b49dff910c1b9d3580
osmonalieve <osmonaliev00700799@icloud.com>
2024-11-25 22:07:18 +0600

    vip commit

---
+++ gulbubuu.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById("revealButton").addEventListener("click", function () {
+  const hiddenMessage = document.getElementById("hiddenMessage");
+  hiddenMessage.style.display = "block";
+});
